0d6eb1bb5f5a0abd86dd778b7f2aa2e146b7262d
patelvandan11 <vandna120ptl@gmail.com>
2024-09-02 09:36:02 +0530

    new

---
+++ newP/Abstract.java
@@ -0,0 +1,17 @@
+abstract class Bike{ 
+    abstract void run();
+    void m(){
+        System.out.println("nvuvhu");
+    }
+   } 
+
+class Honda4 extends Bike{ 
+   void run(){System.out.println("running safely");} 
+
+   public static void main(String args[]){ 
+   Bike obj = new Honda4(); 
+   obj.run(); 
+   obj.m();
+    
+   } 
+   }
---
+++ newP/Super.java
@@ -0,0 +1,17 @@
+//  ovverride
+class Animal{ 
+    void eat(){System.out.println("eating...");} 
+    } 
+    class Dog extends Animal{ 
+    void eat(){System.out.println("eating bread...");} 
+    void bark(){System.out.println("barking...");} 
+    void work(){ 
+    super.eat(); 
+    bark(); 
+    } 
+    } 
+    class Super{ 
+    public static void main(String args[]){ 
+    Dog d=new Dog(); 
+    d.work(); 
+    }}
